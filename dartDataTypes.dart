1c0c03efb5f7cc220dc82f1e850c96828e085032
Kevin Kipkorir <135063319+kevzkip@users.noreply.github.com>
2024-03-04 12:24:07 +0300

    Add files via upload

---
+++ dartDataTypes.dart
@@ -0,0 +1,23 @@
+void main() {
+  String name = "Kevin";
+  print("Hello, my name is $name!");
+  int age = 25;
+  print("I am $age years old.");
+  String country = "Kenya";
+  print("I come from $country!");
+  double height = 1.75;
+  print("My height is $height meters.");
+  List<String> hobbies = ["Reading", "Coding", "Hiking"];
+  print("My hobbies are: $hobbies ");
+  Map<String, dynamic> personInfo = {
+    "name": "Kevin",
+    "age": 20,
+    "isStudent": true,
+  };
+  print("Person Info: $personInfo");
+  int apples = 10;
+  int bananas = 5;
+  int totalFruits = apples + bananas;
+  print("Total fruits: $totalFruits");
+}
+
